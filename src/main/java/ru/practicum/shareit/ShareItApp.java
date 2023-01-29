f043628756c8f146dd63e127f714b78d2fbab895
Oleg Khilko <khilkoleg@gmail.com>
2023-01-29 12:59:18 +0300

    change application to yml

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -1,13 +1,11 @@
 package ru.practicum.shareit;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class ShareItApp {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ShareItApp.class, args);
 	}
-
 }
